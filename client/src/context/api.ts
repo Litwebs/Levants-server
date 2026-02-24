bc882212621caab77fa9df971888815935dcdcc6
Hesam Mousavi <hesammousavi@Mac.lan>
2026-02-24 23:33:07 +0000

    url chanmged

--- client/src/context/api.ts
+++ client/src/context/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://api.levantsdairy.co.uk/api",
+  baseURL: "/api",
   // baseURL: "http://localhost:5001/api",,
   withCredentials: true,
 });
